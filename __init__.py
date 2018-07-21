9bad708bfd1b3542b0fd68f676280519aad369de
jjchshayan <j23chk24m@yahoo.com>
2018-07-21 17:38:54 +0430

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,67 +1,45 @@
-#!/usr/bin/env python
+import logging
+import os
 
-'''Using Webhook and self-signed certificate'''
-
-# This file is an annotated example of a webhook based bot for
-# telegram. It does not do anything useful, other than provide a quick
-# template for whipping up a testbot. Basically, fill in the CONFIG
-# section and run it.
-# Dependencies (use pip to install them):
-# - python-telegram-bot: https://github.com/leandrotoledo/python-telegram-bot
-# - Flask              : http://flask.pocoo.org/
-# Self-signed SSL certificate (make sure 'Common Name' matches your FQDN):
-# $ openssl req -new -x509 -nodes -newkey rsa:1024 -keyout server.key -out server.crt -days 3650
-# You can test SSL handshake running this script and trying to connect using wget:
-# $ wget -O /dev/null https://$HOST:$PORT/
-import time
-from flask import Flask, request
-
-import telegram
-
-# CONFIG
-from telegram.ext import Updater
-
-TOKEN    = '545193892:AAF-i-kxjJBeEiVXL1PokHCCEGNnQ1sOXFo'
-HOST     = 'shayantt.herokuapp.com' # Same FQDN used when generating SSL Cert
-PORT     = 8443
-CERT     = 'path/to/ssl/server.crt'
-CERT_KEY = 'path/to/ssl/server.key'
-
-bot = telegram.Bot(TOKEN)
-updater = Updater(token=TOKEN)
-app = Flask(__name__)
-context = (CERT, CERT_KEY)
-
-@app.route('/')
-def hello():
-    # update = telegram.update.Update.de_json(request.get_json(force=True))
-    # bot.sendMessage(chat_id=update.message.chat_id, text='Hello, there')
-    bot.sendMessage(chat_id="@Shayantut", parse_mode="HTML", text="ADASD")
-    return 'Hello World!'
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 
-#@app.route('/' + TOKEN, methods=['GET'])
-#def webhook():
-    #update = telegram.update.Update.de_json(request.get_json(force=True))
-    #bot.send_message(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
-
- #   return 'OK'
+def start(bot, update):
+    update.effective_message.reply_text("Hi!")
 
 
-def setWebhook():
-    print("AA")
-    # add handlers
+def echo(bot, update):
+    update.effective_message.reply_text(update.effective_message.text)
+
+def error(bot, update, error):
+    logger.warning('Update "%s" caused error "%s"', update, error)
+
+
+if __name__ == "__main__":
+    # Set these variable to the appropriate values
+    TOKEN = '545193892:AAF-i-kxjJBeEiVXL1PokHCCEGNnQ1sOXFo'
+    NAME = 'shayantt.herokuapp.com'  # Same FQDN used when generating SSL Cert
+    PORT = 8443
+
+
+
+
+    # Enable logging
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                        level=logging.INFO)
+    logger = logging.getLogger(__name__)
+
+    # Set up the Updater
+    updater = Updater(TOKEN)
+    dp = updater.dispatcher
+    # Add handlers
+    dp.add_handler(CommandHandler('start', start))
+    dp.add_handler(MessageHandler(Filters.text, echo))
+    dp.add_error_handler(error)
+
+    # Start the webhook
     updater.start_webhook(listen="0.0.0.0",
-                           port=PORT,
+                          port=int(PORT),
                           url_path=TOKEN)
-    updater.bot.set_webhook("https://shayantt.herokuapp.com/" + TOKEN)
+    updater.bot.setWebhook("https://{}.herokuapp.com/{}".format(NAME, TOKEN))
     updater.idle()
-
-   # // bot.setWebhook(url='https://%s:%s/%s' % (HOST, PORT, TOKEN)
-   #               )
-
-if __name__ == '__main__':
-    setWebhook()
-    time.sleep(5)
-    #app.run(debug=True, use_reloader=True)
-    app.run(host='0.0.0.0', port=PORT,debug=True)
