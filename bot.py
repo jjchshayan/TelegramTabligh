a4b0d263aac3456f14b998bd81c71d309d591a4b
jjchshayan <j23chk24m@yahoo.com>
2018-07-21 15:13:09 +0430

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,82 +1,44 @@
-from telegram.ext import Updater
-
-updater = Updater(token='545193892:AAF-i-kxjJBeEiVXL1PokHCCEGNnQ1sOXFo')
-dispatcher = updater.dispatcher
-
 import logging
+import os
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 
 def start(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
-
-
-from telegram.ext import CommandHandler
-
-start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
+    update.effective_message.reply_text("Hi!")
 
 
 def echo(bot, update):
-    bot.sendMessage(chat_id="@Shayantut", parse_mode="HTML", text="ADASD")
+    update.effective_message.reply_text(update.effective_message.text)
+
+def error(bot, update, error):
+    logger.warning('Update "%s" caused error "%s"', update, error)
 
 
-from telegram.ext import MessageHandler, Filters
+if __name__ == "__main__":
+    # Set these variable to the appropriate values
+    TOKEN = "Your token from @Botfather"
+    NAME = "The name of your app on Heroku"
 
-echo_handler = MessageHandler(Filters.text, echo)
-dispatcher.add_handler(echo_handler)
+    # Port is given by Heroku
+    PORT = os.environ.get('PORT')
 
+    # Enable logging
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                        level=logging.INFO)
+    logger = logging.getLogger(__name__)
 
-def caps(bot, update, args=''):
-    text_caps = '   '.join(args).upper()
-    bot.send_message(chat_id=update.message.chat_id, text=text_caps)
+    # Set up the Updater
+    updater = Updater(TOKEN)
+    dp = updater.dispatcher
+    # Add handlers
+    dp.add_handler(CommandHandler('start', start))
+    dp.add_handler(MessageHandler(Filters.text, echo))
+    dp.add_error_handler(error)
 
-
-caps_handler = CommandHandler('caps', caps, pass_args=True)
-dispatcher.add_handler(caps_handler)
-
-from telegram import InlineQueryResultArticle, InputTextMessageContent
-
-
-def inline_caps(bot, update):
-    query = update.inline_query.query
-    if not query:
-        return
-    results = list()
-    results.append(
-        InlineQueryResultArticle(
-            id=query.upper(),
-            title='Caps',
-            input_message_content=InputTextMessageContent(query.upper())
-        )
-    )
-    bot.answer_inline_query(update.inline_query.id, results)
-
-
-from telegram.ext import InlineQueryHandler
-
-inline_caps_handler = InlineQueryHandler(inline_caps)
-dispatcher.add_handler(inline_caps_handler)
-
-
-def unknown(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text="Sorry, I didn't understand that command.")
-
-
-unknown_handler = MessageHandler(Filters.command, unknown)
-dispatcher.add_handler(unknown_handler)
-
-
-TOKEN    = '545193892:AAF-i-kxjJBeEiVXL1PokHCCEGNnQ1sOXFo'
-HOST     = 'shayantt.herokuapp.com' # Same FQDN used when generating SSL Cert
-PORT     = 8443
-# updater.start_webhook(listen="0.0.0.0",
-#                       port=PORT,
-#                       # url_path=TOKEN)
-# updater.bot.set_webhook("https://shayantt.herokuapp.com/" + TOKEN)
-# updater.idle()
-
-
-updater.start_polling()
-
+    # Start the webhook
+    updater.start_webhook(listen="0.0.0.0",
+                          port=int(PORT),
+                          url_path=TOKEN)
+    updater.bot.setWebhook("https://{}.herokuapp.com/{}".format(NAME, TOKEN))
+    updater.idle()
