d0c482cc5409e69cfa4d7b317c5781aa99bfb2a9
jjchshayan <j23chk24m@yahoo.com>
2018-07-21 17:35:49 +0430

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -52,8 +52,8 @@ def setWebhook():
     print("AA")
     # add handlers
     updater.start_webhook(listen="0.0.0.0",
-    #                       port=PORT,
-    #                       url_path=TOKEN)
+                           port=PORT,
+                          url_path=TOKEN)
     updater.bot.set_webhook("https://shayantt.herokuapp.com/" + TOKEN)
     updater.idle()
 
