8fb2fe325807da26079c3410772f6cb9f0d269de
jjchshayan <j23chk24m@yahoo.com>
2018-07-21 17:47:02 +0430

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -37,10 +37,13 @@ if __name__ == "__main__":
     dp.add_handler(MessageHandler(Filters.text, echo))
     dp.add_error_handler(error)
 
+    
+    updater.start_polling()
+
     # Start the webhook
-    updater.start_webhook(listen="0.0.0.0",
-                          port=int(PORT),
-                          url_path=TOKEN)
-    updater.bot.setWebhook("https://{}.herokuapp.com/{}".format(NAME, TOKEN))
-    print("SSS");
-    updater.idle()
+    #updater.start_webhook(listen="0.0.0.0",
+     #                     port=int(PORT),
+    #                     url_path=TOKEN)
+    #updater.bot.setWebhook("https://{}.herokuapp.com/{}".format(NAME, TOKEN))
+    #print("SSS");
+    #updater.idle()
