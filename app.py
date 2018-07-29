052df8df09e2c3c1ee07ff5fff2861a481efc1d3
jjchshayan <j23chk24m@yahoo.com>
2018-07-30 00:07:31 +0430

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,50 @@
+import logging
+import os
+
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+
+def start(bot, update):
+    update.effective_message.reply_text("Hi!")
+
+
+def echo(bot, update):
+    update.effective_message.reply_text(update.effective_message.text)
+
+def error(bot, update, error):
+    logger.warning('Update "%s" caused error "%s"', update, error)
+
+
+if __name__ == "__main__":
+    # Set these variable to the appropriate values
+    TOKEN = '545193892:AAF-i-kxjJBeEiVXL1PokHCCEGNnQ1sOXFo'
+    NAME = 'shayantt'  # Same FQDN used when generating SSL Cert
+    PORT = 8443
+
+    print("SSS");
+
+
+    # Enable logging
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                        level=logging.INFO)
+    logger = logging.getLogger(__name__)
+
+    # Set up the Updater
+    updater = Updater(TOKEN)
+    dp = updater.dispatcher
+    # Add handlers
+    dp.add_handler(CommandHandler('start', start))
+    dp.add_handler(MessageHandler(Filters.text, echo))
+    dp.add_error_handler(error)
+
+    
+    updater.start_polling()
+
+    # Start the webhook
+    #updater.start_webhook(listen="0.0.0.0",
+     #                     port=int(PORT),
+    #                     url_path=TOKEN)
+    #updater.bot.setWebhook("https://{}.herokuapp.com/{}".format(NAME, TOKEN))
+    #print("SSS");
+    #updater.idle()
 
